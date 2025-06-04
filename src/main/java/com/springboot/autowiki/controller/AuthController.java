609154a986627b82b111279acff3ceb0152314aa
Taras Rudenko <taras.rudenko@crosspay.net>
2025-06-04 15:05:26 +0300

    feat: Login with JWT token generation

---
+++ src/main/java/com/springboot/autowiki/controller/AuthController.java
@@ -0,0 +1,4 @@
+package com.springboot.autowiki.controller;
+
+public class AuthController {
+}
---
+++ src/main/java/com/springboot/autowiki/security/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.springboot.autowiki.security;
+
+public class JwtAuthenticationFilter {
+}
