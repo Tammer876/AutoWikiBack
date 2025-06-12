1e011a18c705248813b0429ef4c40d3408158829
Taras Rudenko <taras.rudenko@crosspay.net>
2025-06-12 16:28:03 +0300

    feat: Backend URL update

--- src/main/java/com/springboot/autowiki/config/OpenApiConfig.java
+++ src/main/java/com/springboot/autowiki/config/OpenApiConfig.java
@@ -1,4 +1,5 @@
 package com.springboot.autowiki.config;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -6,17 +7,22 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server server = new Server()
+                .url("https://autowiki.uk")
+                .description("Production Server");
         return new OpenAPI()
                 .info(new Info()
                         .title("AutoWiki API")
                         .version("1.0.0")
                         .description("Backend API for the AutoWiki catalog site"))
+                .servers(List.of(server))
                 .components(new Components().addSecuritySchemes("bearerAuth",
                 new SecurityScheme()
                         .type(SecurityScheme.Type.HTTP)
